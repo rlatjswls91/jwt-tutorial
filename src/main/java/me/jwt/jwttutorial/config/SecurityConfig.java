7960d58c92b7c1a90d6ac7d88817cb58c4fa2e80
KIMSUNJIN <rlatjswls91@gmail.com>
2021-04-18 23:46:39 +0900

    initial commit

---
+++ src/main/java/me/jwt/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package me.jwt.jwttutorial.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/hello").permitAll().anyRequest().authenticated();
+    }
+}
